709e199967c3cfbf23551a6a10024c3a7c4a73f6
Olga Petelskaya <o.petelskaya@gmail.com>
2022-02-02 19:30:24 +0300

    Refactor hw-4 task Figure

--- src/4/Figure.jsx
+++ src/4/Figure.jsx
@@ -1,66 +1,52 @@
-import { useEffect, useRef } from "react";
+import { useRef, useState } from "react";
 import './Figure.css';
 
 
 const Figure = () => {
-    const refDiv = useRef();
-    let timerID;
-    let isDown;
-
-    const insertFigure = (top, left) => {
-
-        refDiv.current.innerHTML = `<div>Figure Component</div>`;
-        let divFigure = refDiv.current.childNodes[0];
-
-        divFigure.className = "divFigure";
-        let docHeight = window.innerHeight;
-        let docWidth = window.innerWidth;
-        let divFigureHeight = divFigure.offsetHeight;
-        let divFigureWidth = divFigure.offsetWidth;
-        let heightMax = docHeight - divFigureHeight;
-        let widthMax = docWidth - divFigureWidth;
-        divFigure.style.position = "absolute";
-
-        divFigure.style.marginTop =
-            top ? (top - divFigureHeight / 2 + "px") : (Math.floor(Math.random() * heightMax) + "px");
-        divFigure.style.marginLeft =
-            left ? (left - divFigureWidth / 2 + "px") : (Math.floor(Math.random() * widthMax) + "px");
-    };
-
-    useEffect(() => {
-        insertFigure();
-    }, []);
+    const [isDown, setIsDown] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const timerID = useRef();
 
     const handleMouseOver = () => {
         if (!isDown) {
-            timerID = setTimeout(() => {
-                insertFigure()
+
+            timerID.current = setTimeout(() => {
+                setPosition({
+                    x: Math.floor(Math.random() * (window.innerWidth - 100)),
+                    y: Math.floor(Math.random() * (window.innerHeight - 100))
+                });
             }, 300);
         };
     };
 
     const handleMouseDown = () => {
-        clearTimeout(timerID);
-        isDown = true;
+        clearTimeout(timerID.current);
+        setIsDown(true);
     };
 
-    const handleMouseOut = (event) => {
+    const handleMouseMove = (event) => {
         if (isDown) {
-            insertFigure(event.clientY, event.clientX);
+            setPosition({ x: (event.pageX - 50), y: event.pageY - 50 });
+            console.log('handleMouseMove');
         };
     };
 
     const handleMouseUp = () => {
-        isDown = false;
+        setIsDown(false);
+        console.log('handleMouseUp');
     };
 
-    return <div
-        onMouseOver={handleMouseOver}
-        onMouseDown={handleMouseDown}
-        onMouseOut={handleMouseOut}
-        onMouseUp={handleMouseUp}
-        ref={refDiv}>
-    </div>
+    return (
+        <div className='container' onMouseMove={handleMouseMove}>
+            <div className='divFigure'
+                style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+                onMouseOver={handleMouseOver}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}>
+                Figure Component
+            </div>
+        </div>
+    );
 };
 
 
