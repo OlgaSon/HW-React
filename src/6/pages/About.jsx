ee4247b7d4019806bc813c93baa533b807d0fcb3
Olga Petelskaya <o.petelskaya@gmail.com>
2022-02-08 20:10:12 +0300

    Add 5: Routes

---
+++ src/6/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return <div>
+        <h3>About us</h3>
+        <p>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia saepe eaque earum? Doloremque, excepturi libero eius quod quam accusamus culpa dolorum exercitationem veritatis ab facere suscipit, expedita dolore maxime voluptatem.</p>
+    </div>
+}
+
+export default About
---
+++ src/6/pages/Articles.jsx
@@ -0,0 +1,10 @@
+const Articles = () => {
+    return <section>
+        <h3>There is an articles section</h3>
+        <article>Article 1. Lorem ipsum dolor sit amet.</article>
+        <article>Article 2. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur, quos.</article>
+        <article>Article 3. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum dicta eius obcaecati ratione placeat voluptatem.</article>
+    </section>
+}
+
+export default Articles
